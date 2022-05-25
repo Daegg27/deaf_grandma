ad18215f396a7fbe4f9fb09f757e36485e423b4f
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-25 13:26:32 -0700

    Completed python

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,31 @@
 def deaf_grandma():
 
+   
+    print("HEY KID!")
+    
+    number_of_goodbyes = 0
+    
+    while number_of_goodbyes < 2:
+        user_input = input("Enter Input: ")
+        
+        if user_input == "GOODBYE!" and number_of_goodbyes < 1:
+            print("LEAVING SO SOON?")
+            number_of_goodbyes = 1
+        elif user_input == "GOODBYE!" and number_of_goodbyes == 1:
+            print("LATER, SKATER!")
+            number_of_goodbyes = 2
+        elif len(user_input) == 0:
+            print("WHAT?!")
+            number_of_goodbyes = 0
+        elif user_input.isupper():
+            print("NO, NOT SINCE 1946")
+            number_of_goodbyes = 0
+        elif user_input.isupper() == False:
+            print("SPEAK UP, KID!")
+            number_of_goodbyes = 0
+
+
+
 
 
 deaf_grandma()
